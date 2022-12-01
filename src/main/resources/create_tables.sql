e1a475e3110e43f7518c40e7c30b68f7f7761261
Rachana Devi <rachanadevi18@gmail.com>
2022-12-01 11:48:46 +0530

    Add postgres table, database creation scripts | Rachana

---
+++ src/main/resources/create_tables.sql
@@ -0,0 +1,2 @@
+CREATE TABLE consultants(id             serial PRIMARY KEY,name           VARCHAR(255),specialization VARCHAR(255),place          VARCHAR(255));
+
---
+++ src/main/resources/fill_tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO consultants (name, specialization, place) VALUES('Tom', 'Washing machine', 'Pune');
+INSERT INTO consultants (name, specialization, place) VALUES('Kate', 'Television', 'Mumbai');
+
