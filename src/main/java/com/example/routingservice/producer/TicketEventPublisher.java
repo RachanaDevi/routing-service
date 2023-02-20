0362040dccb3119681d46c812a063a76504bbe25
Rachana Devi <rachanadevi18@gmail.com>
2023-02-20 14:09:56 +0530

    Add Ticket Publisher

--- src/main/java/com/example/routingservice/producer/TicketEventPublisher.java
+++ src/main/java/com/example/routingservice/producer/TicketEventPublisher.java
@@ -1,24 +1,24 @@
-//package com.example.routingservice.producer;
-//
-//import com.example.routingservice.model.TicketEvent;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.support.SendResult;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.concurrent.CompletableFuture;
-//
-//import static com.example.routingservice.constants.KafkaConfigConstants.MESSAGE_TOPIC;
-//
-//@Component
-//public class TicketEventPublisher {
-//
-//    private final KafkaTemplate<String, TicketEvent> kafkaTemplate;
-//
-//    public TicketEventPublisher(KafkaTemplate<String, TicketEvent> kafkaTemplate) {
-//        this.kafkaTemplate = kafkaTemplate;
-//    }
-//
-//    public CompletableFuture<SendResult<String, TicketEvent>> publish(TicketEvent ticketEvent) {
-//        return kafkaTemplate.send(MESSAGE_TOPIC, ticketEvent);
-//    }
-//}
+package com.example.routingservice.producer;
+
+import com.example.routingservice.event.TicketCreated;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.example.routingservice.constants.KafkaConfigConstants.TICKET_SERVICE_TOPIC;
+
+@Component
+public class TicketEventPublisher {
+
+    private final KafkaTemplate<String, TicketCreated> kafkaTemplate;
+
+    public TicketEventPublisher(KafkaTemplate<String, TicketCreated> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public CompletableFuture<SendResult<String, TicketCreated>> publish(TicketCreated ticketEvent) {
+        return kafkaTemplate.send(TICKET_SERVICE_TOPIC, ticketEvent);
+    }
+}
